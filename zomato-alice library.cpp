9de25ae20258e55ddf31df3bf030a70be4d42f92
shashankgangwar <82611799+shashankgangwar@users.noreply.github.com>
2021-10-18 13:58:05 +0530

    zomato

---
+++ zomato-alice library.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+string solve(string& s){
+	int n=s.size();
+	if(n==2) return "";
+	stack<string> st;
+	string temp;
+	for(int i=0;i<n;i++){
+		if(s[i]=='/'){
+			if(temp.size()>0){
+				st.push(temp);
+				temp="";
+			}
+			st.push("/");
+		}
+		else if(s[i]==92){
+			while(st.top()!="/"){
+				temp=st.top()+temp;
+				st.pop();
+			}
+			st.pop();
+			reverse(temp.begin(),temp.end());
+			st.push(temp);
+			temp="";
+		}
+		else{
+			temp.push_back(s[i]);
+		}
+	}
+	return st.top();
+}
+int main(){
+	string s;
+	cin>>s;
+	string ans=solve(s);
+	cout<<ans<<endl;
+	return 0;
+}
