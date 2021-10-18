2ea02d8baf3a99de036b982821e0c975d55be71e
shashankgangwar <82611799+shashankgangwar@users.noreply.github.com>
2021-10-18 15:01:58 +0530

    zomato_watchman_hills_number of pairs

---
+++ zomato_capital of hills.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int> A){
+	int n=A.size();
+	stack<pair<int,int> > st;
+	int ans=0;
+	for(int i=0;i<n;i++){
+		int k=1;
+		while(!st.empty() && st.top().first<=A[i]){
+			ans=ans+st.top().second;
+			if(st.top().first==A[i]) k+=st.top().second;
+			st.pop();
+		}
+		if(!st.empty()) ans=ans+st.top().second;
+		st.push({A[i],k});
+	}
+	return ans;
+}
+int main(){
+	cout<<solve({2,3,2,1})<<endl;
+	cout<<solve({3,2,1,3})<<endl;
+	cout<<solve({1,1,1,1})<<endl;
+	cout<<solve({3,1,1,1,12})<<endl;
+}
